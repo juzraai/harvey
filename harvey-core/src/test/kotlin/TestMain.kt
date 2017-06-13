7494b5ac53f4e2136da9d514ac32454cd677768a
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2017-06-13 18:55:37 +0200

    That todo's been done

--- harvey-core/src/test/kotlin/TestMain.kt
+++ harvey-core/src/test/kotlin/TestMain.kt
@@ -8,5 +8,3 @@ fun main(args: Array<String>) {
 	val a = "-v 5 -u root -p root -n test -b test-batch"
 	DefaultHarveyApplication(a.split(' ').toTypedArray()).run()
 }
-
-// TODO example? app with extended Config class - see if it works
